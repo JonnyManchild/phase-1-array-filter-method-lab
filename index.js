f0fdcdcb978baf5e197bdd33c72243649a74a9c2
Jonathan MacDuff <jonathan.macduff@outlook.com>
2024-01-12 18:12:24 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    let upperCaseName = name.toUpperCase();
+    return drivers.filter(function (driver) {
+        return driver.toUpperCase() === upperCaseName
+    });
+};
+
+function fuzzyMatch(drivers, firstLetters) {
+    return drivers.filter(function (driver) {
+        return driver.startsWith(firstLetters)
+    });
+};
+
+function matchName(drivers, name) {
+    return drivers.filter(function (driver) {
+        return driver.name === name
+    });
+};
